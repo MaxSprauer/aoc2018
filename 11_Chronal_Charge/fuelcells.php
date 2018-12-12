b393f415795a34a292a86f24a9bca891dbdf4940
Max Sprauer <Max.Sprauer@quest.com>
2018-12-12 13:00:30 -0500

    Add 10x10 square optimization for part two

--- 11_Chronal_Charge/fuelcells.php
+++ 11_Chronal_Charge/fuelcells.php
@@ -14,7 +14,9 @@ assert(0 == powerLevel(217, 196, 39));
 assert(4 == powerLevel(101, 153, 71));
 
 // partOne(SERIAL);
+$startTime = time();
 partTwo(SERIAL);
+print "Time: " . (time() - $startTime) . "\n";
 
 function partOne($serial) 
 {
@@ -58,11 +60,52 @@ function partTwo($serial)
     // Find subgrid values
     $winner = null;
     $winX = $winY = $winSize = -1;
+    $cache = array();
+    $cacheHits = 0;
+
+    // Pre-cache 10x10 blocks
+    $size = 10;
+    for ($x = WIDTH - $size + 1; $x >= 1; $x--) {
+        for ($y = HEIGHT - $size + 1; $y >= 1; $y--) {
+            $cache["$x,$y,10"] = subgridValue($grid, $x, $y, 10, 10);
+        }
+    }
+
+    print "Entries in cache: " . count($cache) . "\n";
 
     for ($size = 1; $size <= WIDTH; $size++) {
-        for ($x = 1; $x <= WIDTH - $size + 1; $x++) {
-            for ($y = 1; $y <= HEIGHT - $size + 1; $y++) {
-                $val = subgridValue($grid, $x, $y, $size);
+        for ($x = WIDTH - $size + 1; $x >= 1; $x--) {
+            for ($y = HEIGHT - $size + 1; $y >= 1; $y--) {
+
+                // Cache subgrids of size 10
+                if ($size < 10) {
+                    $val = subgridValue($grid, $x, $y, $size, $size);
+                } else if ($size == 10) {
+                    $coord = "$x,$y,10";
+                    assert(isset($cache[$coord]), $coord);
+                    $cacheHits++;
+                    $val = $cache[$coord];
+                } else if ($size > 10) {
+                    $blocksToUse = intdiv($size, 10);
+                    $overflow = $size % 10;
+                    $val = 0;
+
+                    // First calculate "overflow" outside of 10x10 blocks
+                    $val += subgridValue($grid, $x, $y, $overflow, $overflow);
+                    $val += subgridValue($grid, $x + $overflow, $y, $blocksToUse * 10, $overflow);
+                    $val += subgridValue($grid, $x, $y + $overflow, $overflow, $blocksToUse * 10);
+
+                    // Then add 10x10 blocks
+                    for ($cacheX = 0; $cacheX < $blocksToUse; $cacheX++) {
+                        for ($cacheY = 0; $cacheY < $blocksToUse; $cacheY++) {
+                            $coord = implode(',', [$x + $overflow + ($cacheX * 10), $y + $overflow  + ($cacheY * 10), 10]);
+                            assert(isset($cache[$coord]), "$coord $size $blocksToUse $overflow");
+                            $cacheHits++;
+                            $val += $cache[$coord];
+                        }
+                    }
+                }
+
                 // printf("(%3d, %3d): %5d\n", $x, $y, $val);
                 if (is_null($winner) || $val >= $winner) {
                     $winner = $val;
@@ -74,16 +117,21 @@ function partTwo($serial)
         }
     }
 
-    print "Winner: $winner ($winX, $winY, $winSize)\n";
+    print "Winner: $winner ($winX, $winY, $winSize), $cacheHits\n";
 }
 
 
-function  subgridValue($grid, $startX, $startY, $size = SUBGRID_WIDTH)
+function subgridValue($grid, $startX, $startY, $sizeX = SUBGRID_WIDTH, $sizeY = SUBGRID_HEIGHT)
 {
     $sum = 0;
 
-    for ($x = $startX; $x < $startX + $size; $x++) {
-        for ($y = $startY; $y < $startY + $size; $y++) {
+    assert($startX + $sizeX - 1 <= 300, "$startX $sizeX");
+    assert($startY + $sizeY - 1 <= 300, "$startY $sizeY");
+
+    // printf("%3d, %3d, %3d, %3d\n", $startX, $startY, $sizeX, $sizeY);
+
+    for ($x = $startX; $x < $startX + $sizeX; $x++) {
+        for ($y = $startY; $y < $startY + $sizeY; $y++) {
             $sum += $grid[$x][$y];
         }
     }
@@ -101,7 +149,7 @@ function powerLevel($x, $y, $serial)
     if ($powerLevel < 100) {
         $powerLevel = 0;
     } else {
-        $powerLevel = substr($powerLevel, -3, 1);
+        $powerLevel = intdiv(($powerLevel % 1000), 100);
     }
 
     return $powerLevel - 5;
