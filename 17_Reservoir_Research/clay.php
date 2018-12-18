3b3261b7293598b72972521f9716169763ac3568
Max Sprauer <max-github@sprauer.org>
2018-12-17 22:49:14 -0500

    Day 17: Checkpoint progress.

--- 17_Reservoir_Research/clay.php
+++ 17_Reservoir_Research/clay.php
@@ -1,6 +1,7 @@
 <?php
 
 // Copyright 2018 Max Sprauer
+// This is bad.  It should probably be wrapped up in a class.  Instead we've got functions and globals.
 
 $input = file('input.txt');
 
@@ -61,7 +62,9 @@ foreach ($input as $line) {
 
 // print_r($map);
 print "x: $xMin - $xMax, y: $yMin - $yMax\n";
-iterate($map, 60);
+
+$streamHeads = array(new StreamHead(500, 1));
+iterate($map, $argv[1]);
 printMap($map);
 
 
@@ -84,44 +87,133 @@ function printMap($map)
 
 function iterate(&$map, $iterations)
 {
+    global $streamHeads;
     $count = 0;
-    $heads = array([500, 0]);
-
-    // head can be sand or clay or a stream
 
     do {
-        list($x, $y) = array_shift($heads);
+        $sh = array_shift($streamHeads);
+        $f = "action_{$sh->state}";
+        $newState = $f($map, $sh);
+        assert(!empty($newState));
+        $sh->state = $newState;
+        $count++;
 
-        // Open sand below, turn it into a stream
-        if (!isset($map[$x][$y + 1])) {
-            $map[$x][$y + 1] = '|';
-            array_push($heads, [$x, $y + 1]);
-            $count++;
-        } else {
-            $below = $map[$x][$y + 1];
-
-            if ($below == '#') {
-                if ($map[$x][$y] != '~' && clayOnBothSides($x, $y)) {
-                    // In a bucket.  Turn this into water.  Try to move left and right.
-                    $map[$x][$y] = '~';
-                    array_push($heads, [$x - 1, $y]);
-                    array_push($heads, [$x + 1, $y]);
-                    $count++;                    
-                }
-            }
-
-
-
-
-
+        if ($sh->state != 'done') {
+            array_push($streamHeads, $sh);
         }
 
-    } while ($count < $iterations);
+    } while ($count < $iterations && !empty($streamHeads));
 }
 
-function clayOnBothSides($x, $y)
+function action_falling(&$map, &$sh)
 {
-    return clayOnLeft($x, $y) && clayOnRight($x, $y);
+    $x = $sh->x;
+    $y = $sh->y;
+    $nextState = null;
+
+    // Fall down
+    while (!isset($map[$x][$y])) { // what if it's |?
+        $map[$x][$y] = '|';
+        $y++;
+    } 
+
+    //TODO: Check done state
+    $y--;
+    $sh->y = $y;
+
+    // Figure out if we're in a bucket
+    $left = clayOnLeft($x, $y);
+    $right = clayOnRight($x, $y);
+
+    if ($left && $right && waterOrClayBelow($left, $right, $y)) {
+        // We're in a bucket
+        $nextState = 'filling';
+        $sh->left = $left;
+        $sh->right = $right;
+    } else {
+        $nextState = 'overflowing';
+    }
+
+    return $nextState;
+}
+
+function action_filling(&$map, &$sh)
+{
+    $x = $sh->x;
+    $y = $sh->y;
+
+    $left = clayOnLeft($x, $y);
+    $right = clayOnRight($x, $y);
+
+    while ($left && $right && $left >= $sh->left && $right <= $sh->right) {
+        for ($i = $left + 1; $i < $right; $i++) {
+            $map[$i][$y] = '~';
+        }
+
+        $y--;
+        $left = clayOnLeft($x, $y);
+        $right = clayOnRight($x, $y);    
+    }
+
+    $sh->y = $y;
+    return 'overflowing';
+}
+
+function action_overflowing(&$map, &$sh)
+{
+    $x = $sh->x;
+    $y = $sh->y;
+    $nextState = null;
+
+    // Overflow left and right until we hit clay or drop.  Each drop can be a new stream head.
+
+    // Find borders
+    $left = clayOnLeft($x, $y);
+    $right = clayOnRight($x, $y);
+
+    $fillLeft = ($left) ? max($left, $sh->left) : $sh->left;
+    $fillRight = ($right) ? min($right, $sh->right) : $sh->right;
+
+    // Fill area between left and right with |
+    for ($i = $fillLeft + 1; $i < $fillRight; $i++) {
+        $map[$i][$y] = '|';
+    }
+
+    if ($left == 0 || $left < $sh->left) {
+        // Drop on the left at x = $sh->left
+
+        // Add one more to cover the wall
+        $map[$sh->left][$y] = '|';
+
+        // Start new stream head
+        addStreamHead($sh->left - 1, $y);
+    }
+
+    if ($right == 0 || $right > $sh->right) {
+        // Drop on the right at x = $sh->right
+
+        // Add one more to cover the wall
+        $map[$sh->right][$y] = '|';
+
+        // Start new stream head
+        addStreamHead($sh->right + 1, $y);
+    }
+
+    return 'done';  // But we added one or two new stream heads
+}
+
+function addStreamHead($x, $y)
+{
+    global $streamHeads;
+
+    foreach ($streamHeads as $sh) {
+        if ($sh->x == $x && $sh->y == $y) {
+            print "Dropped stream head at $x, $y\n";
+            return;
+        }
+    }
+
+    $streamHeads[] = new StreamHead($x, $y);
 }
 
 function clayOnLeft($x, $y)
@@ -130,12 +222,12 @@ function clayOnLeft($x, $y)
 
     for ($i = $x; $i > $xMin; $i--) {
         if (isset($map[$i][$y]) && $map[$i][$y] == '#') {
-            return true;
+            return $i;
             break;
         }
     }
 
-    return false;
+    return 0;
 }
 
 function clayOnRight($x, $y)
@@ -144,17 +236,36 @@ function clayOnRight($x, $y)
 
     for ($i = $x; $i < $xMax; $i++) {
         if (isset($map[$i][$y]) && $map[$i][$y] == '#') {
-            return true;
+            return $i;
         }
     }
 
-    return false;
+    return 0;
 }
 
-function waterOrClayBelow($x, $y)
+function waterOrClayBelow($x1, $x2, $y)
 {
+    global $map;
 
+    for ($x = $x1; $x <= $x2; $x++) {
+        if (!isset($map[$x][$y+1])) {
+            return false;
+        }
 
+    }
+
+    return true;
 }
 
+class StreamHead
+{
+    public $x, $y;
+    public $state;
 
+    function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->state = 'falling';
+    }
+}
